93465b358474dd96a14896ecd119dde3ce3a1ee5
VicTheM <victorychibuike121@gmail.com>
2025-08-14 15:18:07 +0100

    Added static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type config struct {
+	addr    string
+	invoker string
+}
+
 func main() {
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":4001", "Port server will listen on")
+	flag.StringVar(&cfg.invoker, "invoker", "Victory", "The person who started the application")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "[ERROR]\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
 	// Registering routes
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on port :4001...")
-	log.Println("Server is running!")
+	infoLog.Printf("This application was started by %s", cfg.invoker)
 
-	err := http.ListenAndServe(":4001", mux)
+	err := http.ListenAndServe(cfg.addr, mux)
 
-	log.Fatal(err)
+	infoLog.Printf("Server is running on port %s.", cfg.addr)
+	errorLog.Fatal(err)
 }
